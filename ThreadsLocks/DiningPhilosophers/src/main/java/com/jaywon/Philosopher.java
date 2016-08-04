13ea289c28df06d7a0d18403df2ebd6e85cc6101
Jay Won <jason@sudokrew.com>
2016-08-03 21:26:45 -1000

    First day complete.

---
+++ ThreadsLocks/DiningPhilosophers/src/main/java/com/jaywon/Philosopher.java
@@ -0,0 +1,28 @@
+class Philosopher extends Thread {
+  private Chopstick first, second;
+  private Random random;
+
+  public Philosopher(Chopstick left, Chopstick right) {
+
+    if(left.getId() < right.getId()) {
+      first = left; second = right;
+    } else {
+      first = right; second = left; 
+    }
+
+    random = new Random(); 
+  }
+
+  public void run() {
+    try {
+      while(true) {
+        Thread.sleep(random.nextInt(1000));  //Think for a while
+        synchronized(first) {                  // Grab first chopstick
+          synchronized(second) {               // Grab second chopstick
+            Thread.sleep(random.nextInt(1000)); //Eat for a while
+          }
+        }
+      }
+    } catch(InterruptedException e) {}
+  }
+}
